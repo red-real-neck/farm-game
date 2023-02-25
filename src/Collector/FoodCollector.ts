5b802d4ae228bff97b5706a5c79953c526c86892
Ruslan <faleot.karas@gmail.com>
2023-02-25 19:05:28 +0300

    resources bar

--- src/Collector/FoodCollector.ts
+++ src/Collector/FoodCollector.ts
@@ -4,7 +4,7 @@ export class FoodCollector {
   private static instance: FoodCollector;
   private EGG: number = 0;
   private MILK: number = 0;
-  private WHEAT: number = 0;
+  private WHEAT: number = 5;
 
   private constructor() {}
 
@@ -18,9 +18,6 @@ export class FoodCollector {
   public addItem(item: Food) {
     this[item.type]++;
     if (item.type === FoodType.WHEAT) this[item.type]++;
-
-    console.log("this.EGG:", this.EGG);
-    console.log("this.MILK:", this.MILK);
-    console.log("this.WHEAT:", this.WHEAT);
+    // this.updateUI();
   }
 }
